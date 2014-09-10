e5633dbf9b653dc88ebc9c9f61aec7afbd78e77f
Stephan Smith <Stephan.Smith.BC93@gmail.com>
2014-09-10 18:07:13 +0100

    Tweak

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/'));
 
 var port = process.env.PORT || 3000
 app.listen(port);
